4082c360d3b27a959b7410c9f84c47ea61137d32
ajaythapliyal <ajaythapliyal1703@gmail.com>
2023-12-19 09:23:45 +0530

    fetches movie poster

--- src/components/movies.tsx
+++ src/components/movies.tsx
@@ -1,12 +1,15 @@
 import { getMovies } from "../data-service.js";
+import styles from "./movies.module.css";
 
 export default async function Movies() {
   const movies = await getMovies();
   return (
-    <>
+    <main className={styles.moviesContainer}>
       {movies.map((movie) => (
-        <div>{movie.title}</div>
+        <div key={movie.id}>
+          <img src={movie.posterUrl} />
+        </div>
       ))}
-    </>
+    </main>
   );
 }
--- src/data-service.ts
+++ src/data-service.ts
@@ -1,8 +1,25 @@
-export async function getMovies() {
+interface MovieResponse {
+  id: number;
+  title: string;
+  poster_path: string;
+}
+
+interface Movie {
+  id: number;
+  title: string;
+  posterUrl: string;
+}
+
+export async function getMovies(): Promise<Array<Movie>> {
   const MOVIES_URL = `${import.meta.env.VITE_BASE_URL}/discover/movie?api_key=${
     import.meta.env.VITE_API_KEY
   }&include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`;
   const response = await fetch(MOVIES_URL);
   const responseData = await response.json();
-  return responseData.results as Array<{ id: number; title: string }>;
+  const movieResponse = responseData.results as Array<MovieResponse>;
+  return movieResponse.map((movie) => ({
+    id: movie.id,
+    title: movie.title,
+    posterUrl: `${import.meta.env.VITE_BASE_IMG_URL}/w200/${movie.poster_path}`,
+  }));
 }
